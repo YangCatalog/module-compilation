0f159c12558f6216ca88198cad4a6be1c419088c
BohdanKonovalenko <bodiakonovalenko@gmail.com>
2022-11-09 16:19:46 +0200

    fix: fixed coverage badge issue

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -40,3 +40,63 @@ class TestUtility(unittest.TestCase):
 
         result = u.module_or_submodule(os.path.join(self.resource_path, 'module_or_submodule/neither.yang'))
         self.assertEqual(result, 'wrong file')
+
+    def test_dict_to_list(self):
+        result = u.dict_to_list({'foo': ['bar', 'foobar'], 'boo': ['far', 'boofar']}, is_rfc=False)
+        self.assertEqual(result, [['foo', 'bar', 'foobar'], ['boo', 'far', 'boofar']])
+
+        result = u.dict_to_list({'foo': ['bar', 'foobar'], 'boo': ['far', 'boofar']}, is_rfc=True)
+        self.assertEqual(result, [['foo', ['bar', 'foobar']], ['boo', ['far', 'boofar']]])
+
+    def test_list_br_html_addition(self):
+        result = u.list_br_html_addition([['foo\n', 'bar\n'], [42, 'foo\nbar\n'], []])
+        self.assertEqual(result, [['foo<br>', 'bar<br>'], ['foo<br>bar<br>'], []])
+
+    def test_resolve_maturity_level(self):
+        result = u._resolve_maturity_level(None, 'fake-ietf-document')
+        self.assertEqual(result, 'not-applicable')
+
+        result = u._resolve_maturity_level(u.IETF.RFC, 'foo-document-name')
+        self.assertEqual(result, 'ratified')
+
+        result = u._resolve_maturity_level(u.IETF.DRAFT, 'draft-ietf-new-thing')
+        self.assertEqual(result, 'adopted')
+
+        result = u._resolve_maturity_level(u.IETF.DRAFT, 'draft-new-thing')
+        self.assertEqual(result, 'initial')
+
+    def test_resolve_working_group(self):
+        result = u._resolve_working_group('iana-crypt-hash@2014-08-06', u.IETF.RFC, 'iana-crypt-hash')
+        self.assertEqual(result, 'NETMOD')
+
+        result = u._resolve_working_group('ietf-restconf@2017-01-26', u.IETF.RFC, 'ietf-restconf')
+        self.assertEqual(result, 'NETCONF')
+
+        result = u._resolve_working_group('draft-foo-groupname-bar@2017-01-26', u.IETF.DRAFT, 'draft-foo-groupname-bar')
+        self.assertEqual(result, 'groupname')
+
+    def test_path_in_dir(self):
+        result = u._path_in_dir(os.path.join(self.resource_path, 'path_in_dir/foo.yang'))
+        self.assertEqual(result, os.path.join(self.resource_path, 'path_in_dir/foo.yang'))
+
+        result = u._path_in_dir(os.path.join(self.resource_path, 'path_in_dir/bar.yang'))
+        self.assertEqual(result, os.path.join(self.resource_path, 'path_in_dir/subdir/bar.yang'))
+
+        result = u._path_in_dir(os.path.join(self.resource_path, 'path_in_dir/foobar.yang'))
+        self.assertEqual(result, os.path.join(self.resource_path, 'path_in_dir/subdir/subsubdir/foobar.yang'))
+
+        result = u._path_in_dir(os.path.join(self.resource_path, 'path_in_dir/boofar.yang'))
+        self.assertEqual(result, os.path.join(self.resource_path, 'path_in_dir/boofar.yang'))
+
+    def test_number_that_passed_compilation(self):
+        result = u.number_that_passed_compilation(
+            {
+                'foo': ['test', 'stuff', 'PASSED', 'more stuff'],
+                'bar': ['test', 'stuff', 'FAILED', 'more stuff'],
+                'foobar': ['test', 'stuff', 'PASSED WITH WARNINGS', 'more stuff'],
+                'boofar': ['test', 'stuff', 'PASSED', 'more stuff'],
+            },
+            2,
+            'PASSED',
+        )
+        self.assertEqual(result, 2)
